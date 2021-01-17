4e0b567856d6e9c3590785821870b3a52a81084e
Matias Jalava <matias@elm.fi>
2021-01-17 18:58:03 +0200

    trying to implement firebase

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import CustomCard from 'components/CustomCard';
 import styles from 'styles/styles';
 import ProjectTable from 'components/ProjectTable';
+import db from 'firebase';
 
-const App = ()=> {
-  return (
-    <div style={styles.background}>
-      <header>
-        <h1>Under construction</h1>
-        <p>
-          <a href='https://github.com/matiaselm'>Matias'</a> portfolio. I'll be collecting all my done projects and own stuff here
+const App = () => {
+
+  useEffect(() => {
+    // Add a new document in collection "cities"
+    db.collection("cities").doc("LA").set({
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA"
+    }).then(() => {
+      console.log("Document successfully written!");
+    })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+  }, [])
+
+  return (<div style={styles.background}>
+    <header>
+      <h1>Under construction</h1>
+      <p>
+        <a href='https://github.com/matiaselm'>Matias'</a> portfolio. I'll be collecting all my done projects and own stuff here
         </p>
-      </header>
-      <body>
-        {/* Container to hold all info cards table-like */}
-        <ProjectTable />
-        <p>to-do: <br />
-            - Span cards to multiple rows <br />
-            - Create file containing info on all projects and write about the most important ones <br></br>
+    </header>
+    <body>
+      {/* Container to hold all info cards table-like */}
+      <ProjectTable />
+      <p>to-do: <br />
+            - Firestore db for project cards <br></br>
             - Add personal info, maybe to header</p>
-      </body>
-    </div>
+    </body>
+  </div>
   );
 }
 
