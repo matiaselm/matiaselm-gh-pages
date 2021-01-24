63ae2e9ce362ad3d580d2963d008775530b77889
matiaselm <matias@elm.fi>
2021-01-24 20:51:03 +0200

    rewrote welcome message, updated to-do

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -7,8 +7,9 @@ const TopBar = () => {
         <a href="https://github.com/matiaselm" style={{ color: 'white', position: 'absolute', right: 8, top: 8, fontSize: '2ch' }}>My GitHub</a>
         <p>
             Thanks for stopping by! I'll be collecting all my
-            done projects and other own stuff here in these cards. 
-            This list is not yet complete, so feel free to visit my GitHub
+            done projects and other own stuff here in these cards. <br/>
+            Click on their images to be taken on their project-sites.
+            This list is not yet complete, so feel free to visit my github
         </p>
     </div>)
 }
